b3192d547e29fa8cd77417c4502ac1c0eab362db
YashwanthKasula25 <yashwanthkasula2504@gmail.com>
2026-03-05 17:18:33 +0530

    updated

--- Backend/slotbooking/settings.py
+++ Backend/slotbooking/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)&4xx+(&j7f$i=c&$wv7))e8kdcjj96n4t2jftwwt7r^!r*z5d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
